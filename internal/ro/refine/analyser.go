1df2758c9be121d7c1216fe668217ae12c3afbd9
代波 <dbstar@y1cloud.com>
2021-12-01 15:59:17 +0800

    精炼分析

--- internal/ro/refine/analyser.go
+++ internal/ro/refine/analyser.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/pkg/errors"
-	"log"
 	"os"
 	"sort"
 	"strconv"
@@ -16,83 +15,62 @@ type WideCount struct {
 	c int
 }
 
+type WideCountPair struct {
+	s string
+	c int
+	o *WideCount
+	x *WideCount
+}
+
+func (p *WideCountPair) String() string {
+	if p.o != nil {
+		return fmt.Sprintf("%s-O: %0.2f%%, %d", p.s, float32(p.o.c*100)/float32(p.c), p.c)
+	} else if p.x != nil {
+		return fmt.Sprintf("%s-X: %0.2f%%, %d", p.s, float32(p.x.c*100)/float32(p.c), p.c)
+	} else {
+		return fmt.Sprintf("%s: %d", p.s, p.c)
+	}
+}
+
+type History struct {
+	file       string
+	startLevel int
+	start      *Resource
+	times      int
+	endLevel   int
+	cost       *Resource
+	records    []Record
+}
+
 type Analyser struct {
+	histories map[string]*History
 }
 
 func NewAnalyse() *Analyser {
-	return &Analyser{}
+	return &Analyser{histories: make(map[string]*History)}
 }
 
-func (a *Analyser) analyse(file string, wide, startLevel int, start *Resource) error {
-	if readFile, err := os.Open(file); err != nil {
-		return err
-	} else {
-		defer readFile.Close()
+func (a *Analyser) analyse(wide int) error {
+	times, succeed, failed, broken, wideCounts := 0, make([]int, 15), make([]int, 15), make([]int, 15), make(map[string]int)
 
-		fmt.Printf("开始 - 精炼等级: %d, %s\n", startLevel, start)
-
-		level, times, cost := startLevel, 0, &Resource{}
-		succeed, failed, broken := make([]int, 15), make([]int, 15), make([]int, 15)
+	for _, history := range a.histories {
+		times += history.times
 		var modes []string
-
-		fileScanner := bufio.NewScanner(readFile)
-		fileScanner.Split(bufio.ScanLines)
-		for fileScanner.Scan() {
-			line := fileScanner.Text()
-			parts := strings.Split(line, ",")
-
-			if len(parts) == 2 {
-				mode := parts[1]
-				if currentLevel, err := strconv.Atoi(parts[0]); err != nil {
-					log.Fatalln(err)
-				} else if level != currentLevel {
-					log.Fatalf("level:%d, currentLevel: %d\n", level, currentLevel)
-				} else {
-					times++
-					fmt.Printf("%d - 等级：%d, %s", times, level, start.sub(cost))
-					cost.refine(level)
-					switch mode {
-					case "+":
-						succeed[level]++
-						level++
-						modes = append(modes, "O")
-					case "-":
-						failed[level]++
-						level--
-						modes = append(modes, "X")
-					case "x":
-						broken[level]++
-						level--
-						modes = append(modes, "X")
-					}
-					fmt.Printf(" ---> 结果：%s, 等级：%d, %s\n", mode, level, start.sub(cost))
-					if mode == "x" {
-						fmt.Printf("\t修复破损 - 等级：%d, Zeny：%d, 炉灰：%d", level, start.zeny-cost.zeny, start.fire-cost.fire)
-						cost.repair()
-						fmt.Printf(" ---> 炉灰：%d\n", start.fire-cost.fire)
-					}
-					if level == 3 {
-						fmt.Printf("\t安全精炼 - 等级：%d, Zeny：%d, 铝：%d", level, start.zeny-cost.zeny, start.al-cost.al)
-						cost.refine(level)
-						level++
-						fmt.Printf(" ---> 等级：%d, Zeny：%d, 铝：%d\n", level, start.zeny-cost.zeny, start.al-cost.al)
-					}
-				}
+		for _, record := range history.records {
+			switch record.result {
+			case Succeed:
+				succeed[record.level]++
+				modes = append(modes, "O")
+			case Failed:
+				failed[record.level]++
+				modes = append(modes, "X")
+			case Broken:
+				broken[record.level]++
+				modes = append(modes, "X")
 			}
 		}
 
-		fmt.Printf("结束 - 精炼等级: %d, %s\n", level, cost)
-
-		for i := 4; i < 15; i++ {
-			s, f, b := succeed[i], failed[i], broken[i]
-			t := s + f + b
-			if t > 0 {
-				fmt.Printf("精炼: %d, 次数：%d, 成功：%d, 降级：%d, 破损：%d, 成功率：%d, 降级率：%d, 破损率：%d\n", i, t, s, f, b, s*100/t, f*100/(f+b), b*100/(f+b))
-			}
-		}
-
-		modeStr := strings.Join(modes, "")
-		size, wideCounts := len(modeStr), make(map[string]int)
+		size, modeStr := len(modes), strings.Join(modes, "")
 		for i := 0; i <= size-wide; i++ {
 			s := modeStr[i : i+wide]
 			if c, ok := wideCounts[s]; ok {
@@ -101,34 +79,67 @@ func (a *Analyser) analyse(file string, wide, startLevel int, start *Resource) e
 				wideCounts[s] = 1
 			}
 		}
-		var wc []*WideCount
-		for s, c := range wideCounts {
-			//sl := len(s)
-			//if s[sl-2:sl-1] == "O" {
-			wc = append(wc, &WideCount{
+	}
+
+	for i := 4; i < 15; i++ {
+		s, f, b := succeed[i], failed[i], broken[i]
+		t := s + f + b
+		if t > 0 {
+			fmt.Printf("精炼：%d, 次数：%d, 成功：%d, 降级：%d, 破损：%d, 成功率：%0.2f%%, 降级率：%0.2f%%, 破损率：%0.2f%%\n",
+				i, s, t, f, b, float32(s*100)/float32(t), float32(f*100)/float32(f+b), float32(b*100)/float32(f+b))
+		}
+	}
+
+	count, pairs := 0, make(map[string]*WideCountPair)
+	for s, c := range wideCounts {
+		count += c
+		prefix, suffix := s[:wide-1], s[wide-1:]
+		pair, ok := pairs[prefix]
+		if !ok {
+			pair = &WideCountPair{s: prefix}
+			pairs[prefix] = pair
+		}
+		pair.c += c
+		if "O" == suffix {
+			pair.o = &WideCount{
 				s: s,
 				c: c,
-			})
-			//}
-		}
-		sort.Slice(wc, func(i, j int) bool {
-			wci, wcj := *wc[i], *wc[j]
-			if wci.s[:wide-1] == wcj.s[:wide-1] {
-				return wci.c > wcj.c
-			} else {
-				return wci.s[:wide-1] > wcj.s[:wide-1]
 			}
-		})
-		for _, item := range wc {
-			fmt.Printf("%s:\t%d\t%0.2f%%\n", item.s, item.c, float32(item.c*100)/float32(times-wide+1))
+		} else {
+			pair.x = &WideCount{
+				s: s,
+				c: c,
+			}
 		}
-		return nil
 	}
+
+	var pairsArray []*WideCountPair
+	for _, pair := range pairs {
+		pairsArray = append(pairsArray, pair)
+	}
+	sort.Slice(pairsArray, func(i, j int) bool {
+		pairi, pairj := *pairsArray[i], *pairsArray[j]
+		if pairi.o != nil && pairj.o == nil {
+			return true
+		} else if pairi.o == nil && pairj.o != nil {
+			return false
+		} else if pairi.o == nil && pairj.o == nil {
+			return pairi.c > pairj.c
+		} else {
+			ratei := pairi.o.c * 10000 / pairi.c
+			ratej := pairj.o.c * 10000 / pairj.c
+			return ratei > ratej
+		}
+	})
+	for _, pair := range pairsArray {
+		fmt.Printf("%0.2f%% - %s\n", float32(pair.c*100)/float32(count), pair)
+	}
+	return nil
 }
 
-func (a *Analyser) play(file string, startLevel int, start *Resource) ([]Record, *Resource, error) {
+func (a *Analyser) play(file string, startLevel int, start *Resource, debug bool) (*History, error) {
 	if readFile, err := os.Open(file); err != nil {
-		return nil, nil, err
+		return nil, err
 	} else {
 		defer readFile.Close()
 
@@ -143,34 +154,56 @@ func (a *Analyser) play(file string, startLevel int, start *Resource) ([]Record,
 			if parts := strings.Split(fileScanner.Text(), ","); len(parts) == 2 {
 				result := Result(parts[1])
 				if currentLevel, err := strconv.Atoi(parts[0]); err != nil {
-					return nil, nil, err
+					return nil, err
 				} else if level != currentLevel {
-					return nil, nil, errors.Errorf("level:%d, currentLevel: %d\n", level, currentLevel)
+					return nil, errors.Errorf("level:%d, currentLevel: %d\n", level, currentLevel)
 				} else {
-					fmt.Printf("%d - 等级：%d, %s", times, level, start.sub(cost))
+					if debug {
+						fmt.Printf("%d - 等级：%d, %s", times, level, start.sub(cost))
+					}
 
 					cost.refine(level)
 					times, records = times+1, append(records, Record{level: level, result: result})
 					level += result.increment()
 
-					fmt.Printf(" ---> 结果：%s, 等级：%d, %s\n", result, level, start.sub(cost))
+					if debug {
+						fmt.Printf(" ---> 结果：%s, 等级：%d, %s\n", result, level, start.sub(cost))
+					}
 					if result == Broken {
-						fmt.Printf("\t修复破损 - 等级：%d, Zeny：%d, 炉灰：%d", level, start.zeny-cost.zeny, start.fire-cost.fire)
+						if debug {
+							fmt.Printf("\t修复破损 - 等级：%d, Zeny：%d, 炉灰：%d", level, start.zeny-cost.zeny, start.fire-cost.fire)
+						}
 						cost.repair()
-						fmt.Printf(" ---> 炉灰：%d\n", start.fire-cost.fire)
+						if debug {
+							fmt.Printf(" ---> 炉灰：%d\n", start.fire-cost.fire)
+						}
 					}
 					if level == 3 {
-						fmt.Printf("\t安全精炼 - 等级：%d, Zeny：%d, 铝：%d", level, start.zeny-cost.zeny, start.al-cost.al)
+						if debug {
+							fmt.Printf("\t安全精炼 - 等级：%d, Zeny：%d, 铝：%d", level, start.zeny-cost.zeny, start.al-cost.al)
+						}
 						cost.refine(level)
 						level++
-						fmt.Printf(" ---> 等级：%d, Zeny：%d, 铝：%d\n", level, start.zeny-cost.zeny, start.al-cost.al)
+						if debug {
+							fmt.Printf(" ---> 等级：%d, Zeny：%d, 铝：%d\n", level, start.zeny-cost.zeny, start.al-cost.al)
+						}
 					}
 				}
 			}
 		}
 
-		fmt.Printf("结束 - 精炼等级: %d, 花费: %s\n", level, cost)
+		fmt.Printf("结束 - 精炼次数: %d, 最终等级: %d, 花费: %s\n", times, level, cost)
 
-		return records, cost, nil
+		history := &History{
+			file:       file,
+			startLevel: startLevel,
+			start:      start,
+			times:      times,
+			endLevel:   level,
+			cost:       cost,
+			records:    records,
+		}
+		a.histories[file] = history
+		return history, nil
 	}
 }
--- internal/ro/refine/analyser_test.go
+++ internal/ro/refine/analyser_test.go
@@ -4,10 +4,23 @@ import "testing"
 
 func TestAnalyse_analyse(t *testing.T) {
 	analyser := NewAnalyse()
-	//if err := analyser.play("refine1.log", 3, 4, &Resource{zeny: 905725339, al: 7559, alPlus: 77, fire: 234}); err != nil {
-	//	t.Fatal(err)
-	//}
-	if _, _, err := analyser.play("data/refine2.log", 4, &Resource{zeny: 885990627, al: 6340, alPlus: 234, fire: 162}); err != nil {
+	//副手
+	if _, err := analyser.play("data/refine1.log", 4, &Resource{zeny: 905725339, al: 7559, alPlus: 77, fire: 234}, false); err != nil {
 		t.Fatal(err)
 	}
+	//盔甲
+	//if _, err := analyser.play("data/refine2.log", 4, &Resource{zeny: 885990627, al: 6340, alPlus: 234, fire: 162}, false); err != nil {
+	//	t.Fatal(err)
+	//}
+	if err := analyser.analyse(6); err != nil {
+		t.Fatal(err)
+	}
+	//5.09% - XXOXX-O: 65.38%, 26
+	//2.15% - OXXXO-O: 63.64%, 11
+	//3.91% - XXOOX-O: 60.00%, 20
+
+	//4.82% - XXXXO-O: 72.73%, 11
+	//4.82% - OXXXX-O: 63.64%, 11
+	//4.39% - XOOOX-O: 60.00%, 10
+
 }
