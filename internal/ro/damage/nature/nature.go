f265e03125675d2f6ef7b23591b8cc0f3f965b5c
代波 <dbstar@y1cloud.com>
2021-12-03 10:51:43 +0800

    实现属性克制

--- internal/ro/damage/nature/nature.go
+++ internal/ro/damage/nature/nature.go
@@ -12,10 +12,24 @@ const (
 	Fire             // 火
 	Holy             // 圣
 	Dark             // 暗
-	Ghost            // 念
 	Undead           // 不死
+	Ghost            // 念
 	Poison           // 毒
 )
+
+var restraints = [][]float32{
+	//{无,地,风,水,火,圣,暗,不死,念,毒}
+	{1, 1, 1, 1, 1, 1, 1, 1, 0.25, 1},                    //无
+	{1, 0.25, 2, 1, 0.5, 0.75, 1, 1, 1, 0.75},            //地
+	{1, 0.5, 0.25, 2, 1, 0.75, 1, 1, 1, 0.75},            //风
+	{1, 1, 0.5, 0.25, 2, 0.75, 1, 1.5, 1, 0.75},          //水
+	{1, 2, 1, 0.5, 0.25, 0.75, 1, 2, 1, 0.75},            //火
+	{1, 1, 1, 1, 1, 0.25, 2, 2, 1, 1.25},                 //圣
+	{1, 1, 1, 1, 1, 2, 0.25, 0.25, 1, 0.25},              //暗
+	{1, 0.5, 0.5, 0.5, 0.5, 1.75, 0.25, 0.25, 1, 0.25},   //不死
+	{0.25, 1, 1, 1, 1, 0.75, 0.75, 1.75, 2, 1},           //念
+	{1, 1.25, 1.25, 1, 1.25, 0.5, 0.25, 0.25, 0.5, 0.25}, //毒
+}
 
 func (n Nature) String() string {
 	switch n {
@@ -35,13 +49,22 @@ func (n Nature) String() string {
 		return "圣"
 	case Dark:
 		return "暗"
-	case Ghost:
-		return "念"
 	case Undead:
 		return "不死"
+	case Ghost:
+		return "念"
 	case Poison:
 		return "毒"
 	default:
 		return "未知"
 	}
+}
+
+func (n Nature) Restraint(defence Nature) float32 {
+	if n > Unlimited && n <= Poison {
+		if defence > Unlimited && defence <= Poison {
+			return restraints[n-1][defence-1]
+		}
+	}
+	return 1
 }
---
+++ internal/ro/damage/nature/nature_test.go
@@ -0,0 +1,44 @@
+package nature
+
+import "testing"
+
+func TestNature_Restraint(t *testing.T) {
+	tests := []struct {
+		name    string
+		n       Nature
+		defence Nature
+		want    float32
+	}{
+		{
+			name:    "Earth->Wind",
+			n:       Earth,
+			defence: Wind,
+			want:    2,
+		},
+		{
+			name:    "Poison->Ghost",
+			n:       Poison,
+			defence: Ghost,
+			want:    0.5,
+		},
+		{
+			name:    "Poison->Unlimited",
+			n:       Poison,
+			defence: Unlimited,
+			want:    1,
+		},
+		{
+			name:    "Unknown->Earth",
+			n:       13,
+			defence: Earth,
+			want:    1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.n.Restraint(tt.defence); got != tt.want {
+				t.Errorf("Restraint() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
