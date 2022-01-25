1df6718e0380f2de3ccdeda25f62bd6c2e0dc35a
代波 <dbstar@y1cloud.com>
2022-01-25 16:34:27 +0800

    重新定义"种族"维度

--- internal/ro/dimension/race/race.go
+++ internal/ro/dimension/race/race.go
@@ -6,50 +6,51 @@ import "gopkg.in/yaml.v3"
 type Race int
 
 const (
-	Unlimited Race = iota
-	Animal         // 动物
-	Human          // 人形
-	Demon          // 恶魔
-	Plant          // 植物
-	Undead         // 不死
-	Formless       // 无形
-	Fish           // 鱼贝
-	Angel          // 天使
-	Insect         // 昆虫
-	Dragon         // 龙
+	Formless Race = iota // 无形
+	Human                // 人形
+	Plant                // 植物
+	Animal               // 动物
+	Insect               // 昆虫
+	Fish                 // 鱼贝
+	Angel                // 天使
+	Demon                // 恶魔
+	Undead               // 不死
+	Dragon               // 龙
 )
 
 func (r Race) String() string {
 	switch r {
-	case Unlimited:
-		return "不限"
-	case Animal:
-		return "动物"
-	case Human:
-		return "人形"
-	case Demon:
-		return "恶魔"
-	case Plant:
-		return "植物"
-	case Undead:
-		return "不死"
 	case Formless:
 		return "无形"
+	case Human:
+		return "人形"
+	case Plant:
+		return "植物"
+	case Animal:
+		return "动物"
+	case Insect:
+		return "昆虫"
 	case Fish:
 		return "鱼贝"
 	case Angel:
 		return "天使"
-	case Insect:
-		return "昆虫"
+	case Demon:
+		return "恶魔"
+	case Undead:
+		return "不死"
 	case Dragon:
 		return "龙"
 	default:
-		return "未知"
+		if r < 0 {
+			return "不限"
+		} else {
+			return "未知"
+		}
 	}
 }
 
 func (r *Race) UnmarshalYAML(value *yaml.Node) error {
-	for i := Unlimited; i <= Dragon; i++ {
+	for i := Formless; i <= Dragon; i++ {
 		if i.String() == value.Value {
 			*r = i
 			break
