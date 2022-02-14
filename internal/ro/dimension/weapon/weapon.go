58c5ad36dcf4c1c74227ecf1a145c45578ab16d9
代波 <dbstar@y1cloud.com>
2022-02-14 10:26:25 +0800

    "Weapon"维度重新定义

--- internal/ro/dimension/weapon/weapon.go
+++ internal/ro/dimension/weapon/weapon.go
@@ -10,57 +10,100 @@ type Weapon int
 
 const (
 	Unlimited Weapon = iota
-	Empty            //空手
-	Dagger           //短剑
-	Sword            //长剑
 	Spear            //长矛
+	Sword            //长剑
+	Wand             //法杖
 	Katar            //拳刃
-	Glove            //拳套
+	Bow              //弓
 	Blunt            //钝器
 	Axe              //斧子
-	Wand             //法杖
 	Book             //书
-	Bow              //弓
+	Dagger           //短剑
 	Musical          //乐器
 	Whip             //鞭子
-	Dart             //风魔
-	Pistol           //手枪
-	Rifle            //来复枪
+	_
+	Glove  //拳套
+	Dart   //风魔
+	Pistol //手枪
+	Rifle  //来复枪
 )
 
-var restraints = [][]float64{
+var Weapons = []Weapon{
+	Spear,
+	Sword,
+	Wand,
+	Katar,
+	Bow,
+	Blunt,
+	Axe,
+	Book,
+	Dagger,
+	Musical,
+	Whip,
+	Glove,
+	Dart,
+	Pistol,
+	Rifle,
+}
+
+var restraints = map[Weapon][]float64{
 	//{Small,Medium,Large}
-	{1.0, 1.0, 1.0},   //空手
-	{1.0, 0.75, 0.5},  //短剑
-	{0.75, 1.0, 0.75}, //长剑
-	{0.75, 0.75, 1.0}, //长矛
-	{0.75, 1.0, 0.75}, //拳刃
-	{1.0, 0.75, 0.5},  //拳套
-	{0.75, 1.0, 1.0},  //钝器
-	{0.5, 0.75, 1.0},  //斧子
-	{1.0, 1.0, 1.0},   //法杖
-	{1.0, 1.0, 0.5},   //书
-	{1.0, 1.0, 0.75},  //弓
-	{0.75, 1.0, 0.75}, //乐器
-	{0.75, 1.0, 0.75}, //鞭子
-	{1.0, 1.0, 1.0},   //风魔
-	{1.0, 1.0, 1.0},   //手枪
-	{1.0, 1.0, 1.0},   //来复枪
+	Dagger:  {1.0, 0.75, 0.5},  //短剑
+	Sword:   {0.75, 1.0, 0.75}, //长剑
+	Spear:   {0.75, 0.75, 1.0}, //长矛
+	Katar:   {0.75, 1.0, 0.75}, //拳刃
+	Glove:   {1.0, 0.75, 0.5},  //拳套
+	Blunt:   {0.75, 1.0, 1.0},  //钝器
+	Axe:     {0.5, 0.75, 1.0},  //斧子
+	Book:    {1.0, 1.0, 0.5},   //书
+	Bow:     {1.0, 1.0, 0.75},  //弓
+	Musical: {0.75, 1.0, 0.75}, //乐器
+	Whip:    {0.75, 1.0, 0.75}, //鞭子
 }
 
 func (w Weapon) String() string {
 	switch w {
 	case Unlimited:
 		return "不限"
+	case Spear:
+		return "长矛"
+	case Sword:
+		return "长剑"
+	case Wand:
+		return "法杖"
+	case Katar:
+		return "拳刃"
+	case Bow:
+		return "弓"
+	case Blunt:
+		return "钝器"
+	case Axe:
+		return "斧子"
+	case Book:
+		return "书"
+	case Dagger:
+		return "短剑"
+	case Musical:
+		return "乐器"
+	case Whip:
+		return "鞭子"
+	case Glove:
+		return "拳套"
+	case Dart:
+		return "风魔"
+	case Pistol:
+		return "手枪"
+	case Rifle:
+		return "来复枪"
 	default:
 		return "未知"
 	}
 }
 
 func (w Weapon) Restraint(s shape.Shape) float64 {
-	if w > Unlimited && w <= Rifle {
-		if s > shape.Unlimited && s <= shape.Large {
-			return restraints[w-1][s-1]
+	if values, exist := restraints[w]; exist {
+		if s >= shape.Small && s <= shape.Large {
+			return values[s-1]
 		}
 	}
 	return 1
