9a7c8bfe690ac26d7bae3023d05f9221423ac6cf
代波 <dbstar@y1cloud.com>
2022-10-01 23:42:50 +0800

    完善Elemental的方法

--- internal/ys/dimension/elemental/elemental.go
+++ internal/ys/dimension/elemental/elemental.go
@@ -4,41 +4,41 @@ package elemental
 type Elemental int
 
 const (
-	Anemo   Elemental = iota // 风
-	Pyro                     // 火
+	Pyro    Elemental = iota // 火
 	Hydro                    // 水
-	Cryo                     // 冰
-	Electro                  // 雷
-	Geo                      // 岩
 	Dendro                   // 草
+	Electro                  // 雷
+	Anemo                    // 风
+	Cryo                     // 冰
+	Geo                      // 岩
 )
 
-var Elements = []Elemental{
-	Anemo,
+var Elementals = []Elemental{
 	Pyro,
 	Hydro,
-	Cryo,
-	Electro,
-	Geo,
 	Dendro,
+	Electro,
+	Anemo,
+	Cryo,
+	Geo,
 }
 
 func (e Elemental) String() string {
 	switch e {
-	case Anemo:
-		return "风"
 	case Pyro:
 		return "火"
 	case Hydro:
 		return "水"
-	case Cryo:
-		return "冰"
-	case Electro:
-		return "雷"
-	case Geo:
-		return "岩"
 	case Dendro:
 		return "草"
+	case Electro:
+		return "雷"
+	case Anemo:
+		return "风"
+	case Cryo:
+		return "冰"
+	case Geo:
+		return "岩"
 	default:
 		if e < 0 {
 			return "不限"
