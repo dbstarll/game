2dc3a719fbcea50464d1f782949f4334bcb9defc
代波 <dbstar@y1cloud.com>
2022-10-01 23:18:27 +0800

    完善Attribute的方法

--- internal/ys/model/attribute.go
+++ internal/ys/model/attribute.go
@@ -17,6 +17,36 @@ func NewAttribute(point point.Point, value float64) *Attribute {
 	}
 }
 
+func (a *Attribute) Accumulation() AttributeModifier {
+	return func(attributes *Attributes) func() {
+		return attributes.Modify(a)
+	}
+}
+
+func (a *Attribute) GetPoint() point.Point {
+	return a.point
+}
+
+func (a *Attribute) GetValue() float64 {
+	return a.value
+}
+
+func (a *Attribute) IsZero() bool {
+	return a == nil || a.value == 0.0
+}
+
+func (a *Attribute) Clone() *Attribute {
+	return NewAttribute(a.point, a.value)
+}
+
+func (a *Attribute) Reverse() *Attribute {
+	return NewAttribute(a.point, -a.value)
+}
+
+func (a *Attribute) Add(value float64) *Attribute {
+	return NewAttribute(a.point, a.value+value)
+}
+
 func (a *Attribute) String() string {
 	return fmt.Sprintf("%s[%v]", a.point, a.value)
 }
