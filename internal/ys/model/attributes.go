75d3b6817ddb40955387657b45c77b6a5ad0d26f
代波 <dbstar@y1cloud.com>
2022-10-01 23:29:27 +0800

    完善Attributes的方法

--- internal/ys/model/attributes.go
+++ internal/ys/model/attributes.go
@@ -1,120 +1,31 @@
 package model
 
 import (
-	"github.com/dbstarll/game/internal/ys/dimension/elemental"
-	"time"
-)
-
-var (
-	NopAttributeModifier = func(attributes *Attributes) func() {
-		return func() {
-		}
-	}
+	"github.com/dbstarll/game/internal/ys/dimension/attribute/point"
 )
 
 type Attributes struct {
-	Hp            int     // 生命值
-	HpPercentage  float64 // 生命值%
-	Atk           int     // 攻击力
-	AtkPercentage float64 // 攻击力%
-	Def           int     // 防御力
-	DefPercentage float64 // 防御力%
-
-	CriticalRate   float64 // 暴击率%
-	CriticalDamage float64 // 暴击伤害%
-
-	EnergyRecharge   float64 // 元素充能效率%
-	ElementalMastery int     // 元素精通
-
-	HealingBonus         float64 // 治疗加成%
-	IncomingHealingBonus float64 // 受治疗加成%
-
-	CDReduction    float64 // 冷却缩减%
-	ShieldStrength float64 // 护盾强效%
-
-	ElementalDamageBonus map[elemental.Elemental]float64 // 元素伤害加成%
-	ElementalResist      map[elemental.Elemental]float64 // 元素抗性%
-	PhysicalDamageBonus  float64                         // 物理伤害加成%
-	PhysicalResist       float64                         // 物理抗性%
+	values map[point.Point]*Attribute
 }
 
-type AttributeModifier func(attributes *Attributes) func()
+func NewAttributes() *Attributes {
+	return &Attributes{values: make(map[point.Point]*Attribute)}
+}
 
-func MergeAttributes(modifiers ...AttributeModifier) AttributeModifier {
-	return func(attributes *Attributes) func() {
-		size := len(modifiers)
-		cancelList := make([]func(), size)
-		for idx, modifier := range modifiers {
-			cancelList[size-idx-1] = modifier(attributes)
-		}
-		return func() {
-			for _, cancel := range cancelList {
-				cancel()
-			}
-		}
+func (a *Attributes) Modify(attribute *Attribute) func() {
+	if attribute.IsZero() {
+		return NopCallBack
 	}
-}
-
-func Superposition(times int, duration, interval time.Duration, modifier AttributeModifier) AttributeModifier {
-	modifiers := make([]AttributeModifier, times)
-	for i := 0; i < times; i++ {
-		modifiers[i] = modifier
+	point := attribute.GetPoint()
+	if oldValue, exist := a.values[point]; !exist {
+		a.values[point] = attribute.Clone()
+	} else if newValue := oldValue.Add(attribute.GetValue()); newValue.IsZero() {
+		delete(a.values, point)
+	} else {
+		a.values[point] = newValue
 	}
-	return MergeAttributes(modifiers...)
-}
-
-func BaseAttributes(baseHp, baseAtk, baseDef int) AttributeModifier {
-	return func(attributes *Attributes) func() {
-		oldHp, oldAtk, oldDef := attributes.Hp, attributes.Atk, attributes.Def
-		attributes.Hp, attributes.Atk, attributes.Def = baseHp, baseAtk, baseDef
-		return func() {
-			attributes.Hp, attributes.Atk, attributes.Def = oldHp, oldAtk, oldDef
-		}
-	}
-}
-
-func AddAtk(add int) AttributeModifier {
-	return func(attributes *Attributes) func() {
-		attributes.Atk += add
-		return func() {
-			attributes.Atk -= add
-		}
-	}
-}
-
-func AddAtkPercentage(add float64) AttributeModifier {
-	return func(attributes *Attributes) func() {
-		attributes.AtkPercentage += add
-		return func() {
-			attributes.AtkPercentage -= add
-		}
-	}
-}
-
-func AddCriticalRate(add float64) AttributeModifier {
-	return func(attributes *Attributes) func() {
-		attributes.CriticalRate += add
-		return func() {
-			attributes.CriticalRate -= add
-		}
-	}
-}
-
-func AddEnergyRecharge(add float64) AttributeModifier {
-	return func(attributes *Attributes) func() {
-		attributes.EnergyRecharge += add
-		return func() {
-			attributes.EnergyRecharge -= add
-		}
-	}
-}
-
-func AddShieldStrength(add float64) AttributeModifier {
-	return func(attributes *Attributes) func() {
-		attributes.ShieldStrength += add
-		return func() {
-			attributes.ShieldStrength -= add
-		}
+	return func() {
+		a.Modify(attribute.Reverse())
 	}
 }
 
@@ -123,44 +34,9 @@ func (a *Attributes) Apply(modifiers ...AttributeModifier) func() {
 }
 
 func (a *Attributes) Accumulation() AttributeModifier {
-	return func(attributes *Attributes) func() {
-		attributes.Hp += a.Hp
-		attributes.HpPercentage += a.HpPercentage
-		attributes.Atk += a.Atk
-		attributes.AtkPercentage += a.AtkPercentage
-		attributes.Def += a.Def
-		attributes.DefPercentage += a.DefPercentage
-		attributes.CriticalRate += a.CriticalRate
-		attributes.CriticalDamage += a.CriticalDamage
-		attributes.EnergyRecharge += a.EnergyRecharge
-		attributes.ElementalMastery += a.ElementalMastery
-		attributes.HealingBonus += a.HealingBonus
-		attributes.IncomingHealingBonus += a.IncomingHealingBonus
-		attributes.CDReduction += a.CDReduction
-		attributes.ShieldStrength += a.ShieldStrength
-		//attributes.ElementalDamageBonus map[elemental.Elemental]float64 // 元素伤害加成%
-		//attributes.ElementalResist      map[elemental.Elemental]float64 // 元素抗性%
-		attributes.PhysicalDamageBonus += a.PhysicalDamageBonus
-		attributes.PhysicalResist += a.PhysicalResist
-		return func() {
-			attributes.Hp -= a.Hp
-			attributes.HpPercentage -= a.HpPercentage
-			attributes.Atk -= a.Atk
-			attributes.AtkPercentage -= a.AtkPercentage
-			attributes.Def -= a.Def
-			attributes.DefPercentage -= a.DefPercentage
-			attributes.CriticalRate -= a.CriticalRate
-			attributes.CriticalDamage -= a.CriticalDamage
-			attributes.EnergyRecharge -= a.EnergyRecharge
-			attributes.ElementalMastery -= a.ElementalMastery
-			attributes.HealingBonus -= a.HealingBonus
-			attributes.IncomingHealingBonus -= a.IncomingHealingBonus
-			attributes.CDReduction -= a.CDReduction
-			attributes.ShieldStrength -= a.ShieldStrength
-			//attributes.ElementalDamageBonus map[elemental.Elemental]float64 // 元素伤害加成%
-			//attributes.ElementalResist      map[elemental.Elemental]float64 // 元素抗性%
-			attributes.PhysicalDamageBonus -= a.PhysicalDamageBonus
-			attributes.PhysicalResist -= a.PhysicalResist
-		}
+	var modifiers []AttributeModifier
+	for _, attr := range a.values {
+		modifiers = append(modifiers, attr.Accumulation())
 	}
+	return MergeAttributes(modifiers...)
 }
