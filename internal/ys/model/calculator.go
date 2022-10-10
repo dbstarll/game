37066573ed8c1fe38744c277a3c5829551bb7dff
代波 <dbstar@y1cloud.com>
2022-10-10 16:02:57 +0800

    封装Formula类

--- internal/ys/model/calculator.go
+++ internal/ys/model/calculator.go
@@ -2,40 +2,46 @@ package model
 
 import (
 	"fmt"
-	"github.com/dbstarll/game/internal/ys/dimension/attackMode"
 	"github.com/dbstarll/game/internal/ys/dimension/attribute/point"
 	"github.com/dbstarll/game/internal/ys/dimension/elemental"
 	"go.uber.org/zap"
 )
 
 type Calculator struct {
-	level             int
-	basicAttributes   *Attributes
 	finalAttributes   *Attributes
 	enemy             *Enemy
 	action            *Action
 	infusionElemental elemental.Elemental
 	values            *Values
+	init              map[string]float64
 }
 
 func NewCalculator(character *Character, enemy *Enemy, action *Action, infusionElemental elemental.Elemental) *Calculator {
 	calculator := &Calculator{
-		level:             character.level,
-		basicAttributes:   character.basicAttributes(),
 		finalAttributes:   character.finalAttributes(),
 		enemy:             enemy,
 		action:            action,
 		infusionElemental: infusionElemental,
+		init: map[string]float64{
+			"人物等级": float64(character.level),
+			//"怪物等级":  float64(enemy.level),
+			"人物攻击力": character.base.Get(point.Atk).value,
+			"武器攻击力": character.weapon.base.Get(point.Atk).value,
+		},
 	}
 	calculator.calculate()
 	return calculator
 }
 
 func (c *Calculator) Get(key string) float64 {
-	return c.values.Get(key)
+	if v, exist := c.values.Get(key); exist && v != nil {
+		return v.value
+	} else {
+		return 0
+	}
 }
 
-func (c *Calculator) set(key string, value float64) (float64, bool) {
+func (c *Calculator) set(key string, value float64) *Formula {
 	return c.values.Set(key, value)
 }
 
@@ -50,6 +56,11 @@ func (c *Calculator) prepare(putZero bool) {
 			}
 		}
 	}
+	for key, value := range c.init {
+		if putZero || value != 0 {
+			c.set(key, value)
+		}
+	}
 }
 
 func (c *Calculator) calculate() {
@@ -58,52 +69,51 @@ func (c *Calculator) calculate() {
 	zap.S().Debugf("Action: %s", c.action)
 	zap.S().Debugf("Elemental: %s + %s = %s", c.action.elemental, c.infusionElemental, c.action.elemental.Infusion(c.infusionElemental))
 	zap.S().Debugf("DamageBonusPoint: %s", c.action.elemental.Infusion(c.infusionElemental).DamageBonusPoint())
+	zap.S().Debugf("%s", c.攻击区())
 
 	// 基础伤害区 = 攻击区 * 倍率区
 	//伤害 = (基础伤害区 + 激化区) * 增伤区 * 暴击区 * 增幅区 * 防御区 * 抗性区
 
-	c.set("人物等级", float64(c.level))
-	c.set("怪物等级", float64(c.enemy.level))
-	c.set("怪物防御%", c.enemy.base.Get(point.DefPercentage).value/100)
-	c.set("基础攻击力", c.basicAttributes.Get(point.Atk).value)
-
-	// 基础区
-	c.set("额外攻击力", c.Get("基础攻击力")*c.Get("攻击力%")+c.Get("攻击力"))
-	c.set("总攻击力", c.Get("基础攻击力")+c.Get("额外攻击力"))
-
-	// 暴击区
-	if c.Get("暴击率") > 1.0 {
-		c.set("暴击率", 1.0)
-	}
-	c.set("暴击收益", 1+c.Get("暴击率")*c.Get("暴击伤害"))
-	c.set("暴伤倍率", 1+c.Get("暴击伤害"))
-
-	// 防御区
-	c.set("穿防系数", 1-c.Get("无视防御"))
-	c.set("减防系数", 1-c.Get("防御减免")+c.Get("怪物防御%"))
-	c.set("防御系数", c.Get("穿防系数")*c.Get("减防系数")*(c.Get("怪物等级")+100))
-	c.set("防御承伤", (c.Get("人物等级")+100)/(c.Get("人物等级")+100+c.Get("防御系数")))
-
-	// 基础伤害
-	c.set("基础伤害", c.Get("总攻击力")*c.Get("防御承伤"))
-	c.set("基础伤害(平均)", c.Get("基础伤害")*c.Get("暴击收益"))
-	c.set("基础伤害(暴击)", c.Get("基础伤害")*c.Get("暴伤倍率"))
-
-	// 增伤区
-	switch c.action.mode {
-	case attackMode.NormalAttack:
-		c.set("普通攻击增伤", 1+c.Get(c.action.elemental.Infusion(c.infusionElemental).DamageBonusPoint().String())+c.Get("普通攻击伤害加成")+c.Get("元素影响增伤")+c.Get("伤害加成"))
-		break
-	case attackMode.ChargedAttack:
-		c.set("重击增伤", 1+c.Get(c.action.elemental.Infusion(c.infusionElemental).DamageBonusPoint().String())+c.Get("重击伤害加成")+c.Get("元素影响增伤")+c.Get("伤害加成"))
-		break
-	case attackMode.ElementalSkill:
-		c.set("元素战技增伤", 1+c.Get(c.action.elemental.DamageBonusPoint().String())+c.Get("元素战技伤害加成")+c.Get("元素影响增伤")+c.Get("伤害加成"))
-		break
-	case attackMode.ElementalBurst:
-		c.set("元素爆发增伤", 1+c.Get(c.action.elemental.DamageBonusPoint().String())+c.Get("元素爆发伤害加成")+c.Get("元素影响增伤")+c.Get("伤害加成"))
-		break
-	}
+	//c.set("怪物防御%", c.enemy.base.Get(point.DefPercentage).value/100)
+	//c.set("基础攻击力", c.basicAttributes.Get(point.Atk).value)
+	//
+	//// 基础区
+	//c.set("额外攻击力", c.Get("基础攻击力")*c.Get("攻击力%")+c.Get("攻击力"))
+	//c.set("总攻击力", c.Get("基础攻击力")+c.Get("额外攻击力"))
+	//
+	//// 暴击区
+	//if c.Get("暴击率") > 1.0 {
+	//	c.set("暴击率", 1.0)
+	//}
+	//c.set("暴击收益", 1+c.Get("暴击率")*c.Get("暴击伤害"))
+	//c.set("暴伤倍率", 1+c.Get("暴击伤害"))
+	//
+	//// 防御区
+	//c.set("穿防系数", 1-c.Get("无视防御"))
+	//c.set("减防系数", 1-c.Get("防御减免")+c.Get("怪物防御%"))
+	//c.set("防御系数", c.Get("穿防系数")*c.Get("减防系数")*(c.Get("怪物等级")+100))
+	//c.set("防御承伤", (c.Get("人物等级")+100)/(c.Get("人物等级")+100+c.Get("防御系数")))
+	//
+	//// 基础伤害
+	//c.set("基础伤害", c.Get("总攻击力")*c.Get("防御承伤"))
+	//c.set("基础伤害(平均)", c.Get("基础伤害")*c.Get("暴击收益"))
+	//c.set("基础伤害(暴击)", c.Get("基础伤害")*c.Get("暴伤倍率"))
+	//
+	//// 增伤区
+	//switch c.action.mode {
+	//case attackMode.NormalAttack:
+	//	c.set("普通攻击增伤", 1+c.Get(c.action.elemental.Infusion(c.infusionElemental).DamageBonusPoint().String())+c.Get("普通攻击伤害加成")+c.Get("元素影响增伤")+c.Get("伤害加成"))
+	//	break
+	//case attackMode.ChargedAttack:
+	//	c.set("重击增伤", 1+c.Get(c.action.elemental.Infusion(c.infusionElemental).DamageBonusPoint().String())+c.Get("重击伤害加成")+c.Get("元素影响增伤")+c.Get("伤害加成"))
+	//	break
+	//case attackMode.ElementalSkill:
+	//	c.set("元素战技增伤", 1+c.Get(c.action.elemental.DamageBonusPoint().String())+c.Get("元素战技伤害加成")+c.Get("元素影响增伤")+c.Get("伤害加成"))
+	//	break
+	//case attackMode.ElementalBurst:
+	//	c.set("元素爆发增伤", 1+c.Get(c.action.elemental.DamageBonusPoint().String())+c.Get("元素爆发伤害加成")+c.Get("元素影响增伤")+c.Get("伤害加成"))
+	//	break
+	//}
 
 	//            // 抗性区
 	//            const phyR = c.set("元素抗性",c.Get("怪物元素抗性") - c.Get("元素抗性减免"));
@@ -174,21 +184,27 @@ func (c *Calculator) calculate() {
 	//                    c.set("剧变伤害(" + key + ")",c.Get("剧变基础伤害") * reactions.upheaval.rate[key]);
 	//                });
 	//            }
-	fmt.Printf("basicAttributes: %+v\n", c.basicAttributes)
 	fmt.Printf("finalAttributes: %+v\n", c.finalAttributes)
 }
 
-func (c *Calculator) 攻击区() float64 {
-	return 1
+func (c *Calculator) 攻击区() *Formula {
+	基础攻击力 := c.values.Add("基础攻击力", "人物攻击力", "武器攻击力")
+	//基础攻击力 = 人物攻击力 + 武器攻击力
+	//c.set("基础攻击力", c.basicAttributes.Get(point.Atk).value)
+	//
+	//// 基础区
+	//c.set("额外攻击力", c.Get("基础攻击力")*c.Get("攻击力%")+c.Get("攻击力"))
+	//c.set("总攻击力", c.Get("基础攻击力")+c.Get("额外攻击力"))
+	return 基础攻击力
 }
 
 func (c *Calculator) 倍率区() float64 {
 	return 1
 }
 
-func (c *Calculator) 基础伤害区() float64 {
-	return c.攻击区() * c.倍率区()
-}
+//func (c *Calculator) 基础伤害区() float64 {
+//	return c.攻击区() * c.倍率区()
+//}
 
 func (c *Calculator) String() string {
 	return c.values.String()
--- internal/ys/model/values.go
+++ internal/ys/model/values.go
@@ -3,40 +3,78 @@ package model
 import (
 	"fmt"
 	"go.uber.org/zap"
+	"strings"
 )
 
 type Values struct {
-	values map[string]float64
+	values map[string]*Formula
 }
 
 func NewValues() *Values {
-	return &Values{values: make(map[string]float64)}
+	return &Values{values: make(map[string]*Formula)}
 }
 
-func (v *Values) Set(key string, value float64) (float64, bool) {
-	if oldValue, exist := v.values[key]; exist {
-		v.values[key] = value
-		return oldValue, true
+type Formula struct {
+	key       string
+	value     float64
+	algorithm string
+	refs      *[]*Formula
+}
+
+func (f *Formula) String() string {
+	if len(f.algorithm) == 0 {
+		return fmt.Sprintf("%s[%v]", f.key, f.value)
 	} else {
-		v.values[key] = value
-		return 0, false
+		return fmt.Sprintf("%s[%v] = %s", f.key, f.value, f.algorithm)
 	}
 }
 
-func (v *Values) Get(key string) float64 {
+func (v *Values) set(formula *Formula) *Formula {
+	v.values[formula.key] = formula
+	return formula
+}
+
+func (v *Values) Set(key string, value float64) *Formula {
+	return v.set(&Formula{
+		key:       key,
+		value:     value,
+		algorithm: "",
+	})
+}
+
+func (v *Values) Get(key string) (*Formula, bool) {
 	if value, exist := v.values[key]; exist {
-		return value
+		return value, true
 	} else {
 		zap.S().Warnf("key not exist: %s", key)
-		return 0
+		return nil, false
 	}
+}
+
+func (v *Values) Add(totalKey string, keys ...string) *Formula {
+	totalValue, values, algorithms := 0.0, make([]*Formula, len(keys)), make([]string, len(keys))
+	for idx, key := range keys {
+		if value, exist := v.Get(key); exist && value != nil {
+			values[idx] = value
+			totalValue += value.value
+		} else {
+			values[idx] = v.Set(key, 0)
+		}
+		algorithms[idx] = fmt.Sprintf("%s[%v]", values[idx].key, values[idx].value)
+	}
+	return v.set(&Formula{
+		key:       totalKey,
+		value:     totalValue,
+		algorithm: strings.Join(algorithms, " + "),
+		refs:      &values,
+	})
 }
 
 func (v *Values) String() string {
 	values := make(map[string]float64)
 	for key, val := range v.values {
-		if val != 0 {
-			values[key] = val
+		if val.value != 0 {
+			values[key] = val.value
 		}
 	}
 	return fmt.Sprintf("%v", values)
