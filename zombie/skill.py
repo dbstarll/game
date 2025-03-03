88a804abe0916aa20fa04c9bcedeb00dfa5209dc
dbstarll <dbstarll@gmail.com>
2025-03-03 16:33:53 +0800

    重命名处理过的技能文件

--- zombie/skill.py
+++ zombie/skill.py
@@ -8,14 +8,32 @@ from _skill import load_skills, match_skills_from_screenshot, record_skill
 
 
 def detect_skills(skills_file):
+  matches = 0
   detects = 0
+  skill_names = []
   with Image.open(skills_file) as im:
     for image_index, kind_name, skill_name, kind_image, skill_rect, skill_image in match_skills_from_screenshot(im):
+      matches += 1
+      skill_names.append(skill_name)
       if skill_name is not None:
         detects += 1
       else:
         print(f'\t{image_index}: {kind_name} - {skill_name}, {skill_rect}, {skills_file}')
         record_skill(image_index, kind_name, kind_image, skill_image)
+
+  part = skills_file.split("-")
+  if len(part) == 2:
+    if matches == 0:
+      part.insert(1, 'mismatch')
+      print(f'\tmismatch: {skills_file} -> {"-".join(part)}')
+      os.rename(skills_file, "-".join(part))
+    elif detects == 3:
+      skill_names.insert(0, part[0])
+      skill_names.insert(1, 'full_match')
+      skill_names.append(part[1])
+      os.rename(skills_file, "-".join(skill_names))
+    else:
+      print(f'\tpart detected:{skill_names} -  {skills_file}')
   return detects == 3
 
 
@@ -27,7 +45,7 @@ if __name__ == "__main__":
   matches = 0
   kinds, skills = load_skills()
   for file in os.listdir(f'tmp/{dist}'):
-    if file.startswith('skills-') and file.endswith('.png'):
+    if file.startswith('skills-17') and file.endswith('.png'):
       skills_file = f'tmp/{dist}/{file}'
       total += 1
       if detect_skills(skills_file):
