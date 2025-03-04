c387cb0f3ebbf28e078332dd4fff913d89acb86d
dbstarll <dbstarll@gmail.com>
2025-03-04 15:29:46 +0800

    显示更详细的技能探测结果

--- zombie/skill.py
+++ zombie/skill.py
@@ -7,7 +7,7 @@ from _game import distribute
 from _skill import load_skills, match_skills_from_screenshot, record_skill
 
 
-def detect_skills(skills_file):
+def detect_skills_from_file(skills_file):
   matches = 0
   detects = 0
   skill_names = []
@@ -34,23 +34,27 @@ def detect_skills(skills_file):
       os.rename(skills_file, "-".join(skill_names))
     else:
       print(f'\tpart detected:{skill_names} -  {skills_file}')
-  return detects == 3
+  return matches, detects
 
 
 if __name__ == "__main__":
   dist = distribute(sys.argv, "mp")
   print(f'游戏发行版本: {dist}')
 
-  total = 0
-  matches = 0
+  files, full_matches, part_matches, mismatch = 0, 0, 0, 0
   kinds, skills = load_skills()
   for file in os.listdir(f'tmp/{dist}'):
-    if file.startswith('skills-17') and file.endswith('.png'):
+    if file.startswith('skills-') and file.endswith('.png'):
       skills_file = f'tmp/{dist}/{file}'
-      total += 1
-      if detect_skills(skills_file):
-        matches += 1
-  print(f'matches: {matches}/{total}')
+      files += 1
+      match_skills, detect_skills = detect_skills_from_file(skills_file)
+      if detect_skills == 3:
+        full_matches += 1
+      elif match_skills == 3:
+        part_matches += 1
+      else:
+        mismatch += 1
+  print(f'files: {files}, full_matches: {full_matches}, part_matches: {part_matches}, mismatch: {mismatch}')
   print(f'kinds: {len(kinds)}')
   for kind, kind_skills in skills.items():
     print(f'skills: {kind} - {len(kind_skills)}')
