7d2f453271acce5d7378fde1645134dcc8db32f9
dbstarll <dbstarll@gmail.com>
2025-03-04 09:54:53 +0800

    识别苹果版的寰球救援难度

--- zombie/_locate.py
+++ zombie/_locate.py
@@ -10,14 +10,20 @@ def locate(needle_image, haystack_image, **kwargs):
   if len(kwargs) == 0:
     return pyscreeze.locate(needle_image, haystack_image, **_LOCATE_OPTIONS)
   else:
-    return pyscreeze.locate(needle_image, haystack_image, **kwargs)
+    custom_options = _LOCATE_OPTIONS.copy()
+    for key, value in kwargs.items():
+      custom_options[key] = value
+    return pyscreeze.locate(needle_image, haystack_image, **custom_options)
 
 
 def locate_all(needle_image, haystack_image, **kwargs):
   if len(kwargs) == 0:
     return pyscreeze.locateAll(needle_image, haystack_image, **_LOCATE_OPTIONS)
   else:
-    return pyscreeze.locateAll(needle_image, haystack_image, **kwargs)
+    custom_options = _LOCATE_OPTIONS.copy()
+    for key, value in kwargs.items():
+      custom_options[key] = value
+    return pyscreeze.locateAll(needle_image, haystack_image, **custom_options)
 
 
 def set_game_window(window):
